53e1a576c6fa2c18dd00777106037fe6d9a257e9
Jesse <jesse.zheng17@outlook.com>
2020-12-07 22:07:01 -0700

    review added

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const university = require("./university");
 const department = require("./department");
 const course = require("./course");
-
+const review = require("./review");
 const student = require("./student");
 
 module.exports = (app) => {
   app.use("/university", university);
   app.use("/department", department);
   app.use("/course", course);
+  app.use("/review", review);
   app.use("/student", student);
 };
